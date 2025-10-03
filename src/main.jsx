5be6cbab0f05aaa29352c78b8b0e46ace53432cc
Angie-Crackthecode <angpenaalvarez@crackthecode.la>
2025-10-03 11:53:34 -0500

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Envolver la app con el BrowserRouter */}
+    <BrowserRouter basename="/RUTAS/"> {/* Envolver la app con el BrowserRouter */}
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
